bb596cc15f8058e5b05ee6848c23d4507c7a0da4
JagaranMaharjan <01jagaranmah10@gmail.com>
2020-11-10 16:13:48 +0545

    configured postgresql database, created customer, tag, order and prodcut model, added logo in navigation bar, created superusers, added new customer, tag, product and order data from admin panel, rendering database data in template like customer, product and dashboard and lastly confugured dynamic urls.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Product, Order, Tag
 
 # Register your models here.
+
+# register customer model in admin panel
+admin.site.register(Customer)
+
+# register tag model in admin panel
+admin.site.register(Tag)
+
+# register order model in admin panel
+admin.site.register(Order)
+
+# register product model in admin panel
+admin.site.register(Product)
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 # display dashboard
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    # return all orders from order table
+    orders = Order.objects.all()
+    # return all customers from customer table
+    customers = Customer.objects.all()
+    # count total customers
+    total_customers = customers.count()
+    # count total orders
+    total_orders = orders.count()
+    # count total product delivered
+    total_delivered = orders.filter(status='Delivered').count()
+    # count total pending product to be delivered
+    total_pending = orders.filter(status='Pending').count()
+    # created dictionary to pass value in template
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+               'total_orders': total_orders, 'total_delivered': total_delivered, 'total_pending': total_pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 
 # display product ui
 def products(request):
-    return render(request, 'accounts/products.html')
+    # return all products from product table
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 # display customer ui
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    # return customer details according to customer id
+    customer = Customer.objects.get(id=pk_test)
+    # Returns all orders related to customer
+    orders = customer.order_set.all()
+    # return total orders
+    order_count = orders.count()
+    # created dictionary to pass value in template
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
